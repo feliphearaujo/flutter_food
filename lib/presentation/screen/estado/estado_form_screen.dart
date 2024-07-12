46cb9112ff95fb0d47897994baa36acfae08c066
feliphearaujo <feliphearaujoti@gmail.com>
2024-07-12 19:55:42 -0300

    Campos obrigatórios

--- lib/presentation/screen/estado/estado_form_screen.dart
+++ lib/presentation/screen/estado/estado_form_screen.dart
@@ -14,12 +14,31 @@ class EstadoFormScreen extends StatefulWidget {
 }
 
 class _EstadoFormScreenState extends State<EstadoFormScreen> {
+// Pega estado do formulário (salvar, validar, resetar os dados associados ao Widget Form)
+  final GlobalKey<FormState> _formState = GlobalKey<FormState>();
+  // final para ter apenas uma instância do global key do formulário
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         actions: [
-          IconButton(onPressed:(){}, icon: const Icon(Icons.check)),
+          IconButton(onPressed:(){
+            // O primeiro ! serve para negar as condições dos validators do formulário
+            // O segundo ! é utilizado para acessar classes que podem conter valores nulos
+            if (!_formState.currentState!.validate()){  
+              // Aqui construímos a janela de diálogo
+              showDialog(context: context, builder: (context){
+                return AlertDialog(
+                  title: const Text("Aviso!"),
+                  content: const Text("Preencha os campos obrigatórios!"),
+                  actions: [TextButton(onPressed:(){Navigator.pop(context);} , child: const Text("Ok"))]
+                );
+
+              } );
+            }else{
+
+            }
+          }, icon: const Icon(Icons.check)),
           IconButton(onPressed:(){ Navigator.pop(context); } , icon: const Icon(Icons.cancel))
         ],
         title: const Text('Cadastro Estado'),
@@ -31,22 +50,29 @@ class _EstadoFormScreenState extends State<EstadoFormScreen> {
 Widget _body(BuildContext context) {
   
   return Padding(padding: EdgeInsets.all(16),
-  // Para organizar os componentes verticalmente
-    child: Column(children: [
+    child:Form(
+      key: _formState, 
+      child: Column(children: [
       Row(children: [
-        Flexible(child: TextFormField(),)
+        Flexible(child: TextFormField(
+          decoration: InputDecoration(label: Text("Nome")),
+          validator: (value) {
+            // Verificando se o campo é diferente de nulo e se retirei todos os espaços e está vazio
+            if (value != null && value.trim().isEmpty){
+              return "Campo obrigatório!";
+            }
+          },
+        ),)
       ]),
       const SizedBox(height: 16,),
-      Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+      Row(mainAxisAlignment: MainAxisAlignment.start, children: [
         Flexible(child: TextFormField()),
         const SizedBox(width: 16),
         Flexible(child: TextFormField()),
       ],)
     ]),
+    ) 
   );
-
-}
-  
-
+  }
 
 }
